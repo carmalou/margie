ee4d744d839e07acfc68a8286f164b54f9df69e9
carmalou <carmenbourlonlong@gmail.com>
2017-06-18 18:38:05 -0500

    convert it back to csv and we're done woot

--- convert-data-csv/index.js
+++ convert-data-csv/index.js
@@ -14,19 +14,29 @@ fs.readFile(file, 'utf-8', function(err, data) {
 function parseData(data) {
   var newtmparr = [];
   var tmpArr = data.split(',,,,,,');
+
   for(var i = 0; i < tmpArr.length; i++) {
     newtmparr.push(tmpArr[i].split(','));
   }
+
+  if(newtmparr[newtmparr.length - 1] == '') {
+    newtmparr.pop();
+  }
+
   for(var j = 0; j < newtmparr.length; j++) {
     for(var k = 0; k < newtmparr[j].length; k++) {
       if(newtmparr[j][k].includes('Census Tract')) {
-        console.log('true');
-        console.log(newtmparr[j][k]);
-        newtmparr[j][k].replace(/[^\d.-]/g, '');
-        console.log(newtmparr[j][k]);
+        newtmparr[j][k] = newtmparr[j][k].replace(/[^\d.-]/g, '');
       }
+      if(newtmparr[j][k].includes('County')) {
+        newtmparr[j][k] = newtmparr[j][k].replace(/\s|["']|County/g, '');
+      }
+      newtmparr[j][k] = newtmparr[j][k].replace(/\s|["']/g, '');
     }
   }
+
+  newtmparr.unshift(['Census Track', 'County', 'State']);
+  console.log(newtmparr);
 }
 
 // lastly stream it into new file
