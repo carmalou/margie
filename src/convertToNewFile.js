223647621ccd7c343cf3266851d6e88c0456435c
carmalou <carmenbourlonlong@gmail.com>
2017-09-28 21:58:57 -0500

    got my file

--- src/convertToNewFile.js
+++ src/convertToNewFile.js
@@ -1,26 +1,25 @@
 var fs = require('fs');
-var file1 = require(__dirname + '/cb_2016_40_tract_500k_formatted.json');
-var file2 = require(__dirname + '/census_data_oklahoma.json');
+var file1 = require('./cb_2016_40_tract_500k_formatted.json');
+var file2 = require('./census_data_oklahoma.json');
 
 function loopOverCensusData() {
   for(var i = 0; i < file2.length; i++) {
     loopOverGeoJSON(file2[i]);
   }
+  writeNewFile(file1);
 }
 
 function loopOverGeoJSON(properties) {
   for(var i = 0; i < file1.features.length; i++) {
     if(file1.features[i].properties.NAME == properties.census_track_number) {
-      for(var key in properties) {
-        file1.features[i].properties[key.toUpperCase()] = properties[key];
-      }
+      file1.features[i].properties = Object.assign(file1.features[i].properties, properties);
     }
   }
-  writeNewFile(file1);
 }
 
 function writeNewFile(fileData) {
-  var tmpStr = JSON.stringify(fileData);
+  var tmpStr = JSON.stringify(fileData, null, 2);
+  console.log(tmpStr);
   fs.writeFile('cb_2016_census_and_tract_data.json', tmpStr, 'utf-8');
 }
 
